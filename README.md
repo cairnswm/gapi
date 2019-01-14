934ecec555b2c35a1f1a517fc60915178bd90aaa
cairnswm <cairnswm@eskom.co.za>
2019-01-14 20:47:00 +0200

    First version with fully working Rest API

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Generic API - PHP
 
 # Idea
 
-1. Create a standard REST API that can connect to any table in the database
-2. Make the API Configutrable so through configuration the list of tables can be set, and the fields that can be seen, updated, deleted
-3. Add Call backs for Pre/POST functionality (Allows additional security)
-4. Auto Documnet the API with the correct Call
-5. Modify Get to include Sorting and Paging
-6. Add a Search option that does standard get with parameters. (Can be Post of Get)
+1. Create a standard REST API that can connect to any table in the database (Done)
+2. Make the API Configutrable so through configuration the list of tables can be set, and the fields that can be seen, updated, deleted (Done)
+3. Add Call backs for Pre/POST functionality (Allows additional security) 
+4. Auto Document the API with the correct Call
+5. Modify Get to include Sorting and Paging (Pagination: Done)
+6. Add a Search option that does standard get with parameters. (Can be Post or Get)
 
 # Demos
 
@@ -20,6 +20,43 @@ Create example Javascript files to demo the API
 1. Allow calls to load child objects as Collections within the JSON
 2. Respond with XML instead of JSON
 3. ? Convert to a class so that Callbacks are created as child object functions ?
+
+
+# Installation
+
+Copy the api.php to your server directory. Open the file and set the Database Connection values (server, username, password, schema).
+
+# Usage
+
+Moldify the Config structure with the details of your tables
+
+$config = Array(
+    "messages" => Array(
+					"key" => "id",  <=== Define the key field in the database
+					"select" => Array("chatid","username","message","createddate"), <=== Limit which fields acan be selected
+					"update" => Array("message"), <=== Limit which fields can be updated
+					"delete" => true,  <=== Allow deletions
+					"create" => Array("chatid","username","message")  <=== Limit which fields can be detailed when new record is created (not id in this case is auto)
+				),
+    "user" => Array(
+					"key" => "id",
+					"select" => Array("id","username","fullname"),
+					"update" => Array("fullname"),
+					"delete" => false, <=== Prevent API from deleting
+					"create" => false  <=== Prevent API from creating records
+				),
+	"chats" => Array(
+					"tablename" => "chat",   <=== Note renaming of table - API converts incoming chats to the correct table name chat
+					"key" => "id",
+					"select" => Array("id","name"),
+					"update" => false, <=== Prevent API from updating records
+					"delete" => false,
+					"create" => false
+	)
+);
+
+Note select can also be set to false to prevent selecting of records (eg a monitoring end point where the systems should only be able to create new records)
+
 
 
 
--- api.php
+++ api.php
@@ -2,57 +2,223 @@
 // Based on https://www.leaseweb.com/labs/2015/10/creating-a-simple-rest-api-in-php/
 
 $config = Array(
-  "messages" => Array(
-                        "select" => Array("chatid","username","message","createddate"),
-						"update" => Array("message"),
-						"delete" => false,
-						"create" => Array("chatid","username","message")
-                     ),
-  "user" => Array(
-                        "select" => Array("id","username","fullname"),
-						"update" => Array("fullname")
-                     )
+    "messages" => Array(
+					"tablename" => "messages",
+					"key" => "chatid",
+					"select" => Array("chatid","username","message","createddate"),
+					"update" => Array("message"),
+					"delete" => false,
+					"create" => Array("chatid","username","message")
+				),
+    "user" => Array(
+					"key" => "id",
+					"select" => Array("id","username","fullname"),
+					"update" => Array("fullname"),
+					"delete" => false,
+					"create" => false
+				),
+	"chats" => Array(
+					"tablename" => "chat",
+					"key" => "id",
+					"select" => Array("id","name"),
+					"update" => false,
+					"delete" => false,
+					"create" => false
+	)
 );
 
 function Run($config, $mysqli = null)
 {
+	$info = Array();
 	// get the HTTP method, path and body of the request
 	$method = $_SERVER['REQUEST_METHOD'];
 	$input = getParameters($method);
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-	//var_dump($input);
-	//var_dump($request);
-	
+	$key = null;
+
 	// connect to or reuse the mysql database
 	if ($mysqli == null)
-	{ 	$link = mysqli_connect('localhost', 'root', '', 'eskomemp'); }
+	{ $link = mysqli_connect('localhost', 'root', '', 'eskomemp'); }
 	else
 	{ $link = $mysqli; }
 	 	
 	mysqli_set_charset($link,'utf8');
 	 
 	$table = getTable($config, $request);
-	$key = array_shift($request)+0;
+	if (count($request) > 1) { 	$key = $request[1]; }
+	$info["table"] = $table;
+	$info["key"] = $key;
+	$info["method"] = $method;
+	$info["input"] = $input;
 
-	//var_dump($request);
-	//echo $table;
-	//echo $key;
+	
 	 
+	// create SQL based on HTTP method
+	switch ($method) {
+	  case 'GET':
+		echo returnGET($config, $link, $info); break;
+	  case 'PUT':
+		echo returnPUT($config, $link, $info); break;
+	  case 'POST':
+	    echo returnPOST($config, $link, $info); break;
+	  case 'DELETE':
+		echo returnDELETE($config, $link, $info); break;
+	}
+	 
+	// close mysql connection
+	mysqli_close($link);
+}
+
+function ExecSQL($link,$sql)
+{
+	$result = mysqli_query($link,$sql);
+	 
+	// die if SQL statement failed
+	if (!$result) {
+	  http_response_code(404);
+	  die('Error: '.mysqli_error($link));
+	}
+	return $result;
+}
+
+function getTablename($config,$table)
+{
+	if (isset($config[$table]["tablename"]))
+	{ return $config[$table]["tablename"]; }
+    return $table;
+}
+function getTableKey($config,$table)
+{
+	if (isset($config[$table]["key"]))
+	{ return $config[$table]["key"]; }
+    return "id";
+}
+
+function returnGET($config, $mysqli, $info)
+{
+	if ($config[$info["table"]]["select"] == false)
+	{
+		http_response_code(401);
+		die('Error: Action not allowed');
+	}
+	//echo "returnGet";
+	$table = $info["table"];
+	$tablename = getTablename($config, $info["table"]);
+	$key = $info["key"];
+	// Check for Pagination  limit=20&offset=40
+	$limit = "";
+	//var_dump($_GET);
+	if (isset($_GET["offset"])) { $limit .= $_GET["offset"]; };
+	if (isset($_GET["limit"])) { $limit .= (strlen($limit)>0?',':'').$_GET["limit"]; };
+	$limit = (strlen($limit)>0?'Limit '.$limit:'');
+	
+	$fields = implode(', ', $config[$table]["select"]);
+	$sql = "select $fields from `".$tablename."`".($key?" WHERE ".$config[$table]["key"]."=$key ":" $limit"); 
+	//echo $sql;
+	$result = ExecSQL($mysqli,$sql);
+	$res = "";
+	if (!$info["key"]) $res .= '[';
+	  for ($i=0;$i<mysqli_num_rows($result);$i++) {
+		$res .= ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+	  }
+	  if (!$info["key"]) $res .= ']';
+	return $res;	
+}
+
+function returnPUT($config, $mysqli, $info)
+{
+	if ($config[$info["table"]]["update"] == false)
+	{
+		http_response_code(403);
+		die('Error: Action not allowed');
+	}
+	$table = $info["table"];
+	$tablename = getTablename($config, $info["table"]);
+	$key = $info["key"];
+
+	$set = getSetValues($config, $mysqli, $info);
+	$sql = "update `$tablename` set $set where id=$key"; 
+
+	$result = ExecSQL($mysqli,$sql); 
+	return mysqli_affected_rows($mysqli);
+}
+
+function returnPOST($config, $mysqli, $info)
+{
+	if ($config[$info["table"]]["create"] == false)
+	{
+		http_response_code(403);
+		die('Error: Action not allowed');
+	}
+	$table = $info["table"];
+	$tablename = getTablename($config, $info["table"]);
+	$key = $info["key"];
+
+	$set = getSetValues($config, $mysqli, $info);
+	$sql = "insert into `$table` set $set"; 
+
+	$result = ExecSQL($mysqli,$sql); 
+	return mysqli_insert_id($mysqli);
+}
+
+function returnDELETE($config, $mysqli, $info)
+{
+	if ($config[$info["table"]]["delete"] == false)
+	{
+		http_response_code(403);
+		die('Error: Action not allowed');
+	}
+	$table = $info["table"];
+	$tablename = getTablename($config, $info["table"]);
+	$key = $info["key"];
+
+	$set = getSetValues($config, $mysqli, $info);
+	$sql = "Delete from `$tablename` where id=$key"; 
+
+	$result = ExecSQL($mysqli,$sql); 
+	return mysqli_affected_rows($mysqli);
+}
+
+ 
+function getParameters($method)
+{
+	// PUT variables set in php://input - note the Content-Type must be correct (x-www-form-urlencoded)
+	// POST variables in $_POST
+	$input = null;
+	// Load Parameters into generic variable
+	switch ($method) {
+	case 'PUT':
+	    $put_data = file_get_contents('php://input');
+		parse_str($put_data, $post_vars);
+		$input = $post_vars; break;
+    case 'POST':
+		$input = $_POST; break;
+	}
+	return $input;
+}
+
+function getSetValues($config, $mysqli, $info)
+{
+	$input = $info["input"];
+	$table = $info["table"];
+	$method = $info["method"];
+	$set = '';
+	
 	if (isset($input))
 	{
 		// escape the columns and values from the input object
 		$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
-		$values = array_map(function ($value) use ($link) {
+		$values = array_map(function ($value) use ($mysqli) {
 		  if ($value===null) return null;
-		  return mysqli_real_escape_string($link,(string)$value);
-		},array_values($input));
-		 
+		  return mysqli_real_escape_string($mysqli,(string)$value);
+		},array_values($input));		
+					
+		// Decide which fieldset to use
+		$fieldlist = $config[$table]["create"]; 
+		if ($method == "PUT") { $fieldlist = $config[$table]["update"];  }
+
 		// build the SET part of the SQL command
-		$set = '';
 		for ($i=0;$i<count($columns);$i++) {
-			// Decide which fieldset to use
-			$fieldlist = $config[$table]["create"]; 
-			if ($method == "PUT") { $fieldlist = $config[$table]["update"]; }
 			if (in_array($columns[$i],$fieldlist))
 			{
 				$set.=(strlen($set)>0?',':'').'`'.$columns[$i].'`=';
@@ -66,62 +232,8 @@ function Run($config, $mysqli = null)
 			die();
 		}
 	}
-	 
-	// create SQL based on HTTP method
-	switch ($method) {
-	  case 'GET':
-		$fields = implode(', ', $config[$table]["select"]);
-		$sql = "select $fields from `$table`".($key?" WHERE id=$key":''); break;
-	  case 'PUT':
-		$sql = "update `$table` set $set where id=$key"; break;
-	  case 'POST':
-		$sql = "insert into `$table` set $set"; break;
-	  case 'DELETE':
-		$sql = "delete `$table` where id=$key"; break;
-	}
-	 
-	// excecute SQL statement
-	//echo $sql;
-	$result = mysqli_query($link,$sql);
-	 
-	// die if SQL statement failed
-	if (!$result) {
-	  http_response_code(404);
-	  die('Error: '.mysqli_error());
-	}
-	 
-	// print results, insert id or affected row count
-	if ($method == 'GET') {
-	  if (!$key) echo '[';
-	  for ($i=0;$i<mysqli_num_rows($result);$i++) {
-		echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
-	  }
-	  if (!$key) echo ']';
-	} elseif ($method == 'POST') {
-	  echo mysqli_insert_id($link);
-	} else {
-	  echo mysqli_affected_rows($link);
-	}
-	 
-	// close mysql connection
-	mysqli_close($link);
-}
- 
-function getParameters($method)
-{
-	// PUT variables set in php://input - not the Content-Type must be correct
-	// POST variables in $_POST
-	$input = null;
-	// Load Parameters into generic variable
-	switch ($method) {
-	case 'PUT':
-	    $put_data = file_get_contents('php://input');
-		parse_str($put_data, $post_vars);
-		$input = $post_vars; break;
-    case 'POST':
-		$input = $_POST; break;
-	}
-	return $input;
+
+	return $set;
 }
 
 function getTable($config, $request)
